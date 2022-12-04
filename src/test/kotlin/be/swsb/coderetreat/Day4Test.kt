c686521ecc14e397648dc124338c834dd5982918
Bram Meerten <bram.meerten@aca-it.be>
2022-12-04 09:10:02 +0100

    AOC day 4

---
+++ src/test/kotlin/be/swsb/coderetreat/Day4Test.kt
@@ -0,0 +1,33 @@
+package be.swsb.coderetreat
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import readFile
+
+
+class Day4Test {
+
+    @Test
+    fun `part 1`() {
+        val result = readFile("day4/exampleInput.txt")
+                .map { l -> l.split(",") }
+                .map { pair -> pair
+                        .map { it.split("-") }
+                        .map { (a, b) -> a.toInt() to b.toInt() } }
+                .count { (a, b) -> a.first >= b.first && a.second <= b.second || b.first >= a.first && b.second <= a.second }
+
+        assertEquals(2, result)
+    }
+
+    @Test
+    fun `part 2`() {
+        val result = readFile("day4/exampleInput.txt")
+                .map { l -> l.split(",") }
+                .map { pair -> pair
+                        .map { it.split("-") }
+                        .map { (a, b) -> a.toInt() to b.toInt() } }
+                .count { (a, b) -> a.first >= b.first && a.first <= b.second || b.first >= a.first && b.first <= a.second }
+
+        assertEquals(4, result)
+    }
+}
