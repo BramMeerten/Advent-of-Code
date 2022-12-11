c1232e9f6044e0593ea59976d3d06fdceaaf6db2
Bram Meerten <bram.meerten@aca-it.be>
2022-12-11 17:29:32 +0100

    Advent of Code day 11b

--- src/test/kotlin/be/brammeerten/Day11Test.kt
+++ src/test/kotlin/be/brammeerten/Day11Test.kt
@@ -14,91 +14,93 @@ class Day11Test {
     @Test
     fun `part 1`() {
         val monkeys = parseMonkeys("day11/input.txt")
-        println(monkeys[0].test(47))
+        for (round in (1..20))
+            doRound(monkeys, relieve = true)
 
-        for (round in (1..20)) {
-            doRound(monkeys)
-//            println("After round $round, the monkeys are holding items with these worry levels:")
-//            monkeys.forEachIndexed {i, m ->
-//                println("Monkey $i: " + m.items.map { it.toString() }.joinToString (", "))
-//            }
-//            println()
-        }
-            val counts = monkeys.map { it.count }.sortedDescending()
-
-        Assertions.assertEquals(10605, counts[0] * counts[1])
+        val counts = monkeys.map { it.count }.sortedDescending()
+        Assertions.assertEquals(61503L, counts[0] * counts[1])
     }
 
-    fun doRound(monkeys: List<Monkey>) {
+    @Test
+    fun `part 2`() {
+        val monkeys = parseMonkeys("day11/input.txt")
+        for (round in (1..10000))
+            doRound(monkeys)
+
+        monkeys.forEachIndexed { i, m -> println("Monkey $i: ${m.count}") }
+        val counts = monkeys.map { it.count }.sortedDescending()
+
+        Assertions.assertEquals(14081365540L, counts[0] * counts[1])
+    }
+
+    fun doRound(monkeys: List<Monkey>, relieve: Boolean = false) {
+        val optimizeFactor = calcOptimizeFactor(monkeys)
         monkeys.forEachIndexed { i, monkey ->
-            monkey.items.forEach{ item ->
+            monkey.items.forEach { item ->
                 // Inspect
                 var worry = monkey.operation(item)
                 monkey.count++
-//                println("worry after inspect: $worry")
+
+                // Optimize
+                worry %= optimizeFactor
 
                 // Relieve
-                worry /= 3
-//                println("Relieve: $worry")
+                if (relieve) worry /= 3
 
-                // test and throw
-                val newMonk = monkey.test(worry)
-//                println("Throw to: $newMonk")
-                if (newMonk == i) throw IllegalStateException("Monkey throws to itself")
+                // Test and throw
+                val newMonk = monkey.nextMonkey(worry)
                 monkeys[newMonk].items.add(worry)
             }
             monkey.items.clear()
-//            println("")
         }
     }
 
-    fun parseMonkeys(fileName: String): List<Monkey> {
-        return readFileSplitted(fileName, "\n\n")
-            .map { parseMonkey(it) }
-    }
+    fun parseMonkeys(fileName: String) = readFileSplitted(fileName, "\n\n").map { parseMonkey(it) }
 
     fun parseMonkey(lines: List<String>): Monkey {
-        if (!(Regex("^Monkey \\d+:$").matches(lines[0]))) throw IllegalStateException("Expected different first line: ${lines[0]}")
-        val items = extractRegexGroups(ITEMS_REGEX, lines[1])[0].split(", ").map { it.toInt() }
-        val operation = readOperation(lines[2])
-        val test = readTest(lines.drop(3))
+        val items = extractRegexGroups(ITEMS_REGEX, lines[1])[0].split(", ").map { it.toLong() }
+        val div = extractRegexGroups(DIV_REGEX, lines[3])[0].toInt()
+        val trueMonkey = extractRegexGroups(TRUE_REGEX, lines[4])[0].toInt()
+        val falseMonkey = extractRegexGroups(FALSE_REGEX, lines[5])[0].toInt()
 
-        return Monkey (operation, test, items.toMutableList())
+        return Monkey(
+            readOperation(lines[2]),
+            div, trueMonkey, falseMonkey,
+            items.toMutableList()
+        )
     }
 
-    fun readOperation(line: String): (Int) -> Int {
+    fun readOperation(line: String): (Long) -> Long {
         val opString = extractRegexGroups(OP_REGEX, line)[0]
         return { old ->
-            val ops = opString.replace("old", ("" + old), false)
-                .split(" ")
-            val start = ops[0].toInt()
+            val ops = opString.replace("old", old.toString(), false).split(" ")
+            val start = ops[0].toLong()
             ops.drop(1)
                 .windowed(2, 2)
                 .fold(start) { result, (op, num) ->
                     when (op) {
-                        "+" -> result + num.toInt()
-                        "*" -> result * num.toInt()
+                        "+" -> result + num.toLong()
+                        "*" -> result * num.toLong()
                         else -> throw IllegalStateException("Unknown operation: $op")
                     }
                 }
         }
     }
 
-    fun readTest(lines: List<String>): (Int) -> Int {
-        val div =  extractRegexGroups(DIV_REGEX, lines[0])[0]
-        val trueMonkey =  extractRegexGroups(TRUE_REGEX, lines[1])[0]
-        val falseMonkey =  extractRegexGroups(FALSE_REGEX, lines[2])[0]
-        return { item ->
-            if (item % div.toInt() == 0) trueMonkey.toInt() else falseMonkey.toInt()
-        }
-    }
+    fun calcOptimizeFactor(monkeys: List<Monkey>) =
+        monkeys.map { it.divByTest }.reduce{a, b -> a * b}
 
     class Monkey(
-        val operation: (Int) -> Int,
-        val test: (Int) -> Int,
-        val items: MutableList<Int>) {
+        val operation: (Long) -> Long,
+        val divByTest: Int,
+        private val monkeyDivTrue: Int,
+        private val monkeyDivFalse: Int,
+        val items: MutableList<Long>) {
+        var count = 0L
 
-        var count = 0
+        fun nextMonkey(item: Long): Int {
+            return if (item % divByTest == 0L) monkeyDivTrue else monkeyDivFalse
+        }
     }
 
 }
