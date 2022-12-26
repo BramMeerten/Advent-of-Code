14829c5af95536f753b6ff44e73c74d11cd3cc91
Bram Meerten <bram.meerten@aca-it.be>
2022-12-26 11:02:11 +0100

    Advent of code day 17 part 2

--- src/test/kotlin/be/brammeerten/Day17Test.kt
+++ src/test/kotlin/be/brammeerten/Day17Test.kt
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import sun.security.provider.SHA
 import kotlin.math.max
+import kotlin.system.exitProcess
+
+val NUM_ROWS_FOR_PATTERN = 100
 
 class Day17Test {
 
@@ -21,27 +24,59 @@ class Day17Test {
         Assertions.assertEquals(3224, simulate(2022, chamberWidth = 7, pattern))
     }
 
-    // TODO
     @Test
     fun `part 2a`() {
         val pattern = readJetPattern(readSingleLine("day17/exampleInput.txt"))
         Assertions.assertEquals(1514285714288L, simulate(1000000000000L, chamberWidth = 7, pattern))
     }
 
+    @Test
+    fun `part 2b`() {
+        val pattern = readJetPattern(readSingleLine("day17/input.txt"))
+        Assertions.assertEquals(1514285714288L, simulate(1000000000000L, chamberWidth = 7, pattern)) // < 1600571428577
+    }
+
     fun readJetPattern(pattern: String): List<C> {
         return pattern.toCharList().map { c -> if (c == '<') C.LEFT else C.RIGHT }
     }
 
-    fun simulate(rounds: Long, chamberWidth: Int, jetPattern: List<C>): Int {
+    fun simulate(rounds: Long, chamberWidth: Int, jetPattern: List<C>): Long {
         val chamber = Chamber(chamberWidth)
+
+        val prevs: HashMap<String, Pair<Int, Long>> = hashMapOf()
+
+        var ugly = 0L
+        var addTo = 0L
 
         var i =0
         var shapeI = 0
-        for (n in 0 until rounds) {
-            i = simulateFallingBlock(chamber, SHAPES[shapeI], jetPattern, i)
-            shapeI = (shapeI+1) % SHAPES.size
+        try {
+            for (n in 0 until rounds) {
+                i = simulateFallingBlock(chamber, SHAPES[shapeI], jetPattern, i)
+                shapeI = (shapeI + 1) % SHAPES.size
+
+                if (chamber.h >= NUM_ROWS_FOR_PATTERN) {
+                    val c = chamber.getCachedState(i, shapeI)
+                    if (prevs.contains(c)) {
+                        var prevHeight = prevs[c]
+                        val growth = chamber.h - prevHeight!!.first
+                        val roundsSinceRepeat = n - prevHeight.second
+                        val roundsLeft = rounds - n
+                        val estimate = chamber.h + ((roundsLeft / roundsSinceRepeat) * growth)
+                        addTo = ((roundsLeft / roundsSinceRepeat) * growth)
+                        ugly = roundsLeft % roundsSinceRepeat
+                        throw IllegalStateException("I FOUND IT curHeight: ${chamber.h} cachedHeight: ${prevHeight} --> $estimate")
+                    }
+                    prevs.put(c, chamber.h to n)
+                }
+            }
+        } catch (e: IllegalStateException) {
+            for (n in 0 until ugly-1) {
+                i = simulateFallingBlock(chamber, SHAPES[shapeI], jetPattern, i)
+                shapeI = (shapeI + 1) % SHAPES.size
+            }
         }
-        return chamber.h
+        return chamber.h + addTo
     }
 
     fun simulateFallingBlock(chamber: Chamber, shape: Shape, jetPattern: List<C>, jetI: Int): Int {
@@ -131,6 +166,21 @@ class Day17Test {
             }
         }
 
+        fun getCachedState(patternPos: Int, shapePos: Int): String {
+//            val c = Array(w){Array(NUM_ROWS_FOR_PATTERN){false} }
+            var s = "$shapePos" + ("$patternPos".padStart(5, '0'))
+            (1 .. NUM_ROWS_FOR_PATTERN).forEach { y ->
+                (0 until w).forEach { x ->
+//                    c[x][NUM_ROWS_FOR_PATTERN-y] = blocks[x][h-y] ?: false
+                    s += if (blocks[x][h-y] ?: false) "#" else "."
+                }
+            }
+            if (h == 89 || h == 36)
+                println(s)
+//            return c.contentHashCode()
+            return s
+        }
+
         fun print() {
             val heighest = max(3, h)
 
@@ -141,4 +191,33 @@ class Day17Test {
             println("+-------+\n")
         }
     }
+
+    data class CachedState(val height: Int, val lastRows: Array<Array<Boolean>>) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as CachedState
+
+            if (!lastRows.contentEquals(other.lastRows)) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            return lastRows.contentHashCode()
+        }
+    }
+
+}
+
+fun printMyDing(a: Array<Array<Boolean>>) {
+    println("${a.contentHashCode()}:")
+    for (y in a.indices) {
+        for (x in a[y].indices) {
+            print(if(a[y][x]) "#" else ".")
+        }
+        println()
+    }
+    println()
 }
