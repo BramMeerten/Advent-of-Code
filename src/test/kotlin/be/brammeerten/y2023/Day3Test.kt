2ebe037dfc7cc192cf0bd84306069529cfc66edb
Bram Meerten <bram.meerten@acagroup.be>
2023-12-03 10:09:20 +0100

    Day 3 2023, add solutions

--- src/test/kotlin/be/brammeerten/y2023/Day3Test.kt
+++ src/test/kotlin/be/brammeerten/y2023/Day3Test.kt
@@ -1,20 +1,16 @@
 package be.brammeerten.y2023
 
 import be.brammeerten.Co
-import be.brammeerten.extractRegexGroups
 import be.brammeerten.readFile
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.util.regex.Pattern
-import kotlin.math.max
-import kotlin.streams.toList
 
 
 class Day3Test {
 
     @Test
     fun `part 1`() {
-        val inputLines = readFile("2023/day3/input.txt")
+        val inputLines = readFile("2023/day3/exampleInput.txt")
         val lines = inputLines
             .map { line ->
                 val numbers = mutableListOf<Pair<Int, String>>()
@@ -44,12 +40,12 @@ class Day3Test {
         }
 
         assertThat(sum).isEqualTo(4361);
-//         assertThat(sum).isEqualTo(2169);
+//         assertThat(sum).isEqualTo(539590);
     }
 
     @Test
     fun `part 2`() {
-        val inputLines = readFile("2023/day3/input.txt")
+        val inputLines = readFile("2023/day3/exampleInput.txt")
         val lines = inputLines
             .map { line ->
                 val numbers = mutableListOf<Pair<Int, String>>()
@@ -78,35 +74,25 @@ class Day3Test {
         }
 
         assertThat(sum).isEqualTo(467835);
-//         assertThat(sum).isEqualTo(2169);
+//         assertThat(sum).isEqualTo(80703636);
     }
 
     private fun getNumbersSurrounding(y: Int, x: Int, lines: List<List<Pair<Int, String>>>): List<Int> {
         val sum = mutableListOf<Int>()
-        if (y-1 >= 0) {
-            sum.addAll(lines[y - 1].filter { line ->
-                val start = line.first
-                val end = line.first + line.second.length-1
-                val result  = (x in start..end || (x-1) in start..end || (x+1) in start..end)
-                result
-            }.map { Integer.parseInt(it.second) })
-        }
-
-        sum.addAll(lines[y].filter { line ->
-            val start = line.first
-            val end = line.first + line.second.length-1
-            val result  = (x in start..end || (x-1) in start..end || (x+1) in start..end)
-            result
-        }.map { Integer.parseInt(it.second) })
-
-        sum.addAll(lines[y + 1].filter { line ->
-            val start = line.first
-            val end = line.first + line.second.length-1
-            val result  = (x in start..end || (x-1) in start..end || (x+1) in start..end)
-            result
-        }.map { Integer.parseInt(it.second) })
+        if (y-1 >= 0) sum.addAll(getNumbersOnLineSurroundingPositionX(x, lines[y - 1]))
+        sum.addAll(getNumbersOnLineSurroundingPositionX(x, lines[y]))
+        if (y+1 < lines.size) sum.addAll(getNumbersOnLineSurroundingPositionX(x, lines[y + 1]))
 
         return sum
+    }
+
+    private fun getNumbersOnLineSurroundingPositionX(x: Int, line: List<Pair<Int, String>>): List<Int> {
+        return line.filter {
+            val start = it.first
+            val end = it.first + it.second.length - 1
+            val result = (x in start..end || (x - 1) in start..end || (x + 1) in start..end)
+            result
+        }.map { Integer.parseInt(it.second) }
     }
 
     private fun isSurrounded(lines: List<String>, row: Int, col: Int): Boolean {
